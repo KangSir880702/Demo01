961f33bec14724a1df34ac705f590abd4f2afa1f
Kangsir <765185665@qq.com>
2020-02-23 10:44:27 +0800

    01:main.c

---
+++ main.cpp
@@ -0,0 +1,10 @@
+ulog_global_filter_kw_set("wifi");
+ulog_global_filter_lvl_set(WARNNING);
+ulog_global_filter_tag_set("wifi");
+ulog_tag_lvl_filter_set("test",LOG_LVL_ERROR)
+pthread_cond_init(pthread_cond_t *cond,const pthread_condattr_t *atrr)
+pthread_cond_destory(pthread_cond_t *cond);
+pthread_cond_wait(pthread_code_t *cond,pthred_mutex_t *mutex)
+pthread_rwlock_rdlock(pthread_rwlock_t *rwlock)
+RTM_EXPORT(function);
+//gitTest 
